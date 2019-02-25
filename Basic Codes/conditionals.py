a261e8e9fb09f32db61c69f411a0ac1c56a65759
david1999erazo <erazo29@hotmail.com>
2019-02-25 09:48:34 -0500

    deletec proof

---
+++ Basic Codes/conditionals.py
@@ -0,0 +1,11 @@
+#initialize variables
+number = 5;
+name = "David"
+age = 19;
+
+#logical expressions
+print(number == 5)
+print(number == 3)
+print(number < 2)
+
+
--- Proof.py
+++
@@ -1,6 +0,0 @@
-print ("ingrese el primer numero")
-x = input()
-print ("ingrese el primer numero")
-y = input()
-sum = x+y
-print (sum)
